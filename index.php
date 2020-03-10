4c73402e0417f5bcb9a69893b2fbbbb783198f01
Admin-Prog <info@info.ru>
2020-03-10 18:10:56 +0300

    p 10.03.2020

---
+++ index.php
@@ -0,0 +1,285 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+    <!--    <link rel="stylesheet" href="css/new-style.css">-->
+    <link rel="stylesheet" href="css/nice-select.css">
+    <!-- Bootstrap CSS -->
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.4/components/navbar/">
+    <link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="Bootstrap/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="Bootstrap/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="Bootstrap/css/bootstrap-grid.min.css">
+    <!-- Bootstrap CSS -->
+    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+    <script src="Bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="Bootstrap/js/bootstrap.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery.nice-select.js"></script>
+
+
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand mon" href="#"><h1 class="mybrand ml-lg-5 mt-lg-4 mb-lg-3">ЖК “КВАРТИРЫ В
+                ДОМОДЕДОВО”</h1></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav col-12 col-lg-9 mon mt-lg-2">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">О ПРОЕКТЕ<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">КВАРТИРЫ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ИПОТЕКА</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">РАСПОЛОЖЕНИЕ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ГАЛЕРЕЯ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">КОНТАКТЫ</a>
+                <li class="nav-item">
+                    <a class="nav-link tel-company disabled" href="#" tabindex="-1" aria-disabled="true"><b>+7 (928)
+                            768-32-39</b></a>
+                </li>
+            </ul>
+            <form class="bt-lup  d-none d-lg-block ml-lg-5">
+                <button class="bt-zvonok" type="submit">Заказать звонок</button>
+            </form>
+        </div>
+    </nav>
+
+</header>
+<section class="fon-block">
+    <div class="container">
+        <div class="col-12 mit">
+            <div class=row>
+                <div class="col-lg-10 col-12 mini_block block_1">Жилищный комплекс</div>
+                <div class="col-lg-10 col-12 mini_block block_2">Южное ДОМОДЕДОВО</div>
+                <div class="col-lg-10 col-12 mini_block block_3">Покупка, продажа, аренда недвижимости в Домодедово
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="container block_podbor">
+    <div class="row block-filter">
+        <div class="col-lg-3 col-md-12 filter">Подобрать квартиру</div>
+        <div class="col-lg-3 col-md-4 filter">
+            <div class="btn-group field group-hat row no-gutters" role="group" aria-label="Basic example">
+                <a class="col-3 bn studio-btn" href="#">Студия</a>
+                <a class="col-3 bn left-btn" href="#">1</a>
+                <a class="col-3 bn center-btn" href="#">2</a>
+                <a class="col-3 bn right-btn" href="#">3+</a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 filter">
+
+            <div class="group-price">
+                <input type="text" name="pf" placeholder="Цена от" class="col-12 col-md-6 deck left-input">
+                <input type="text" name="pf" placeholder="Цена от" class="col-12 col-md-6 deck right-input">
+            </div>
+        </div>
+        <div class="col-lg 3 col-md-4 filter col-button">
+            <button class="button show-button btn-cs">250 предложений</button>
+        </div>
+
+    </div>
+
+</div>
+<section class="info-jk">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 block-info">
+                <div class="row">
+                    <div class="col-12 krt">
+                        <div class="row">
+                            <h3 class="col-12 col-lg-6">жилой комплекс</h3>
+                            <h1 class="col-12 col-lg-9">Южное ДОМОДЕДОВО</h1>
+                            <p class="col-12 col-lg-6">«Новоград Павлино» – это новый жилой комплекс, расположенный в
+                                ближайшем Подмосковье, в
+                                городе
+                                Балашиха, в 7 км от МКАД по Носовихинскому шоссе.</p></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6 left-block-info">
+
+                <div class="row">
+                    <div class="col-12">
+                        <div id="MYcarusel" class="carousel slide" data-interval="false">
+
+                            <div class="carousel-inner slider-info-jk">
+                                <div class="carousel-item active">
+                                    <div class="row">
+                                        <div class="col-6 mel v-red"><span>CРОК СДАЧИ:</span></div>
+                                        <div class="col-6 mel v-red"><span>ДОМОВ:</span></div>
+                                        <div class="col-6 mel v-welt"><span>дом сдан</span></div>
+                                        <div class="col-6 mel v-welt"><span>81 m2</span></div>
+                                        <div class="col-6 mel v-red"><span>ЭТАЖНОСТЬ:</span></div>
+                                        <div class="col-6 mel v-red"><span>ТИП ДОМА:</span></div>
+                                        <div class="col-6 mel v-welt"><span>3.5 </span></div>
+                                        <div class="col-6 mel v-welt"><span>Монолитно-кирпичный</span></div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="row">
+                                        <div class="col-6 mel v-red"><span>CРОК СДАЧИ:</span></div>
+                                        <div class="col-6 mel v-red"><span>ДОМОВ:</span></div>
+                                        <div class="col-6 mel v-welt"><span>дом сдан</span></div>
+                                        <div class="col-6 mel v-welt"><span>81 m2</span></div>
+                                        <div class="col-6 mel v-red"><span>ЭТАЖНОСТЬ:</span></div>
+                                        <div class="col-6 mel v-red"><span>ТИП ДОМА:</span></div>
+                                        <div class="col-6 mel v-welt"><span>3.5 </span></div>
+                                        <div class="col-6 mel v-welt"><span>Монолитно-кирпичный</span></div>
+                                    </div>
+
+
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="row">
+                                        <div class="col-6 mel v-red"><span>CРОК СДАЧИ:</span></div>
+                                        <div class="col-6 mel v-red"><span>ДОМОВ:</span></div>
+                                        <div class="col-6 mel v-welt"><span>дом сдан</span></div>
+                                        <div class="col-6 mel v-welt"><span>81 m2</span></div>
+                                        <div class="col-6 mel v-red"><span>ЭТАЖНОСТЬ:</span></div>
+                                        <div class="col-6 mel v-red"><span>ТИП ДОМА:</span></div>
+                                        <div class="col-6 mel v-welt"><span>3.5 </span></div>
+                                        <div class="col-6 mel v-welt"><span>Монолитно-кирпичный</span></div>
+                                    </div>
+
+
+                                </div>
+                            </div>
+                            <div class="row pols">
+
+                                <a class="col-2 col-lg-1 carousel-control-prev prv" href="#MYcarusel"
+                                   role="button"
+                                   data-slide="prev">
+                                    <span class="carousel-control-prev-icon prv-btn" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <ol class="col-3 col-lg-3  carousel-indicators clik">
+                                    <li data-target="#MYcarusel" data-slide-to="0" class="active"></li>
+                                    <li data-target="#MYcarusel" data-slide-to="1"></li>
+                                    <li data-target="#MYcarusel" data-slide-to="2"></li>
+                                </ol>
+                                <a class="col-2 col-lg-1 carousel-control-next nxt" href="#MYcarusel"
+                                   role="button"
+                                   data-slide="next">
+                                    <span class="carousel-control-next-icon nxt-btn" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6  col-12 right-block-info">
+                <div class="row">
+                    <div class="col-12">
+
+
+                        <div id="multiCarousel" class="carousel slide new-slider" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100 h-440" src="/uploud/image.png" alt="Первый слайд">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100 h-440" src="/uploud/Image1.png" alt="Второй слайд">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100 h-440" src="/uploud/image2.png" alt="Третий слайд">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100 h-440" src="/uploud/Image3.png" alt="Третий слайд">
+                                </div>
+                            </div>
+                            <a class="carousel-control-next" href="#multiCarousel" role="button" data-slide="next">
+                                <span class="new-btn-right" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</section>
+<section class="yut_kvartir">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 col-12 text-center">
+                <h1>УЮТНЫЕ КВАРТИРЫ</h1>
+                <p>Live in a shared building? Make getting together with your neighbours easier by bringing life to
+                    unloved communal spaces.</p>
+            </div>
+            <div class="col-lg-5 col-12 brde-cub"><img src="/uploud/image2.png" alt="Типа ванна"></div>
+            <div class="col-lg-5 col-12 brdr-zero"><img src="/uploud/image.png" alt="#"></div>
+
+
+        </div>
+    </div>
+    <div class="col-lg-6 col-12 brdr-plan">ПЛАНИРОВОЧНЫЕ РЕШЕНИЯ</div>
+</section>
+<section class="podbor-rvrt">
+    <div class="container">
+        <div class="row podbor-filter">
+            <div class="col-lg-3 new_filter text-center bodbor-block-1">Подобрать квартиру</div>
+            <div class="col-lg-3 new_filter text-center">
+                <div class="btn-group field group-hat row no-gutters" role="group" aria-label="Basic example">
+                    <a class="col-3 bn studio-btn" href="#">Студия</a>
+                    <a class="col-3 bn left-btn" href="#">1</a>
+                    <a class="col-3 bn center-btn" href="#">2</a>
+                    <a class="col-3 bn right-btn" href="#">3+</a>
+                </div>
+            </div>
+            <div class="col-lg-3 new_filter">
+                <div class="group-price">
+                    <input type="text" name="pf" placeholder="от 2 384 940" class="col-12 col-md-6 deck left-input">
+                    <input type="text" name="pf" placeholder="до 52 326 975" class="col-12 col-md-6 deck right-input">
+                </div>
+            </div>
+            <div class="col-lg-3 new_filter">
+                <button class="button show-button btn-cs">250 предложений</button>
+            </div>
+        </div>
+
+
+    </div>
+    <div class="green-block row">
+        <div class="col-3"></div>
+        <div class="col-12 col-lg-6">
+            <div class="embed-responsive embed-responsive-16by9 block-video">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/l6FOK4tpiCs" frameborder="0"
+                        allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="col-3"></div>
+    </div>
+</section>
+<section class="map-block">
+    <div class="col-12 text-center"><h1>Москва ближе, чем кажется</h1></div>
+
+
+</section>
+</body>
+</html>
